506f6ff03c5f04b965769a4fb375fcff0a3dcc2e
jem0101 <jem0101@auburn.edu>
2022-12-01 16:18:41 -0600

    Created logging output file after running in /resources/Output/app.log

--- TestOrchestrator4ML-main/label_perturbation_attack/LogHelper.py
+++ TestOrchestrator4ML-main/label_perturbation_attack/LogHelper.py
@@ -1,7 +1,7 @@
 import logging
 
 def getSQALogger():
-    logging.basicConfig(filename='app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename='../../app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     loggerOBJ = logging.getLogger('SQA-Logger')
     loggerOBJ.setLevel(logging.DEBUG)
     return loggerOBJ
